78212297d837fa58759c2c2c51ab71cbce4934d5
Ubuntu <ubuntu@ip-172-31-31-122.us-east-2.compute.internal>
2020-02-18 15:41:43 +0000

    uncompleted assignment

--- code-challenges.js
+++ code-challenges.js
@@ -13,52 +13,92 @@ var num3 = -7
 // Expected output: "-7 is not divisble by three"
 
 
+const divisible = (num) => {
+    if(num % 3 === 0){
+    return num + " is divisible by three"
+} else if (num % 3 >= 1){
+    return num + " is not divisible by three"
+} else if (num % 3 >= -3){
+    return num + " is not divisible by three"
+}else{
+    return "must type in a number"
+}
+}
+console.log(divisible(num1))
+console.log(divisible(num2))
+console.log(divisible(num3))
 
 
 
-// --------------------2) Create a function that takes in the randomNouns variable and returns an array with all the words capitalized.
+// // --------------------2) Create a function that takes in the randomNouns variable and returns an array with all the words capitalized.
 
 var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "temperature", "database"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew", "Temperature", "Database"]
+// // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew", "Temperature", "Database"]
+const capitalizer = (array) => {
+    let eachItem = array.map(value => {
+        return value[0].toUpperCase() + value.substring(1)
+    })
+    return eachItem.join(", ")
+}
+console.log(capitalizer(randomNouns))
 
 
-
-
-
-// --------------------3) Create a function that takes in the mixedDataArray array and returns an array with ONLY NUMBERS sorted from least to greatest.
+// // --------------------3) Create a function that takes in the mixedDataArray array and returns an array with ONLY NUMBERS sorted from least to greatest.
 
 var mixedDataArray = [true, 8, "hello", 90, -8, null, 0, 46, 59, 107, "hey!"]
-// Expected output: [-8, 0, 8, 46, 59, 90, 107]
+// // Expected output: [-8, 0, 8, 46, 59, 90, 107]
+
+const onlyNum = mixedDataArray.filter(function (item) {
+    return (parseInt(item) == item);
+ 
+});
+
+console.log(onlyNum.sort((a, b) => a - b))
+
+// // --------------------4) Create a function that takes in a string and logs the index of the first vowel.
+// // Use the test variables provided.
+
+var vowelTester1 = "learn" 
+// // // Expected output: 1
+var vowelTester2 = "throw"
+// // // Expected output: 3
+
+
+const getIndexOfFirstVowel = (word) => {
+    var vowels = ["a","e","i","o","u"];
+    for (let i=0; i < word.length; i++){
+        for (let j=0; j < vowels.length; j++) {
+           if (word.charAt(i) == vowels[j]){
+              return i;
+           }
+        }
+    }
+}
+
+console.log(getIndexOfFirstVowel(vowelTester1));
+console.log(getIndexOfFirstVowel(vowelTester2));
+
+
+// // --------------------5) Create a function that takes three arguments - two numbers and a mathematical operation (+, -, *, /) and performs the given calculation. If the input tries to divide by 0, return: "Can't divide by 0!"
+
+
+function calculator(a, op, b) {
+if (op == '/' && b === 0) return "Can't divide by 0!"
+if (op == '+') return a + b;
+if (op == '*') return a * b;
+if (op == '-') return a - b;
+if (op == '/') return a / b; 
+}
 
 
 
 
+// // Uncomment and use the following console logs to test your function
+console.log(calculator(3, "*" ,9))
+// // Expected output: 27
 
-// --------------------4) Create a function that takes in a string and logs the index of the first vowel.
-// Use the test variables provided.
+console.log(calculator(16, "+" ,3))
+// // Expected output: 19
 
-var vowelTester1 = "learn"
-// Expected output: 1
-var vowelTester1 = "throw"
-// Expected output: 3
-
-
-
-
-
-
-// --------------------5) Create a function that takes three arguments - two numbers and a mathematical operation (+, -, *, /) and performs the given calculation. If the input tries to divide by 0, return: "Can't divide by 0!"
-
-
-
-
-
-// Uncomment and use the following console logs to test your function
-// console.log(calculator(3, "*" 9))
-// Expected output: 27
-
-// console.log(calculator(16, "+" 3))
-// Expected output: 19
-
-// console.log(calculator(89, "/" 0))
-// Expected output: "Can't divide by 0!"
+console.log(calculator(89, "/" ,0))
+// // Expected output: "Can't divide by 0!"
--- interview-questions.md
+++ interview-questions.md
@@ -4,49 +4,62 @@ Answer the following questions. First, without external resources. Challenge you
 
 1. What does CRUD stand for?
 
-  Your answer:
+  Your answer: Create/React/Update/Delete
 
-  Researched answer:
+  Researched answer: When we are building APIs, we want our models to provide four basic types of functionality. 
+  The model must be able to Create, Read, Update, and Delete resources. A model should have the ability to perform at most these four functions in order to be complete. If an action cannot be described by one of these four operations, 
+  then it should potentially be a model of its own.
 
 
 
 2. What are the 5 HTTP verbs?
 
-  Your answer:
+  Your answer: Post, Get, Delete
 
-  Researched answer:
+  Researched answer: Post, Get, Put, Patch, Delete
 
 
 
 3. When creating a basic (stateless) class component in React, what are the necessary elements needed to render "Hello World" on the page?
 
-  Your answer:
+  Your answer: "class HelloWorld extends React.Component" at the top, render and inside return "Hello World"
 
-  Researched answer:
+  Researched answer:class HelloWorld extends React.Component { 
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;  
+  }
+}
 
 
 
 4. What is JSX?
 
-  Your answer:
+  Your answer: It is the instersection of JavaScript and HTML
 
-  Researched answer:
+  Researched answer: JSX is JavaScript flavored HTML that behaves almost like HTML with a couple small exceptions -- 
+  most notably JSX uses the syntax className rather than HTML's class because class is reserved for HTML.
 
 
 
 5. What is the difference between React state and props?
 
-  Your answer:
+  Your answer: React state is what is in the internal state of the parent, and props are what is passed on to the child.
 
-  Researched answer:
+  Researched answer: State - This is data maintained inside a component. It is local or owned by that specific component. The component itself will update the state using the setState function.
+
+Props - Data passed in from a parent component. props are read-only in the child component that receives them. However, callback functions can also be passed, which can be executed inside the child to initiate an update.
+
+The difference is all about which component owns the data. State is owned locally and updated by the component itself. Props are owned by a parent component and are read-only. Props can only be updated if a callback function is passed to the child to trigger an upstream change.
+
+The state of a parent component can be passed a prop to the child. They are referencing the same value, but only the parent component can update it.
 
 
 
 6. STRETCH: What is hoisting in JavaScript?
 
-  Your answer:
+  Your answer: No clue. 
 
-  Researched answer:
+  Researched answer: Hoisting is JavaScript's default behavior of moving all declarations to the top of the current scope (to the top of the current script or the current function).
 
 
 
